6ee5bc0fef43f63de99b33479d58d48d218218f4
Yazz Atlas <yazz.atlas@hp.com>
2013-03-25 19:04:31 +0000

    Stopping services first

--- bin/install-compute-node.sh
+++ bin/install-compute-node.sh
@@ -6,6 +6,20 @@
 # You must pass the full path to the environments.json for this
 # to work right. 
 #
+NOVA=$(ls -1 /etc/init/nova-* | sed 's#/etc/init/##' | sed 's/\.conf//g')
+
+function stop_services {
+    set +eux
+    local ITEMS=$@
+    for i in ${ITEMS} ; do
+        echo "Stopping: $i"
+        sudo service ${i} stop
+        sleep 1
+    done
+    set -eux
+}
+
 set -exu
+stop_services ${NOVA}
 cd /srv/simple-stack/bin/
 /srv/simple-stack/bin/install-compute ${1}
